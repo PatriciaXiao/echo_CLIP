17a69103e28aadcba84b68f8341d04302ce41e3f
patricia_xiao <patricia.xiao@gmail.com>
2025-03-04 15:26:35 -0600

    Update my_zero_shot_test.py

--- my_zero_shot_test.py
+++ my_zero_shot_test.py
@@ -60,7 +60,7 @@ for idx,(path,split,label) in enumerate(zip(path_list, split_list, label_list)):
         test_video = np.load(path)
         print(test_video.shape) # (3, 60, 256, 256)
         data = list()
-        for i in test_video.shape[0]:
+        for i in range(test_video.shape[0]):
             data.append(test_video[i])
             print(test_video.shape)
         exit(0)
